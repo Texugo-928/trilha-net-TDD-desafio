35ca289b2ed8926b1bfe3e11a4080f173d2e08ae
Cicero V. de Melo Jr <108836762+Texugo-928@users.noreply.github.com>
2024-04-12 22:18:02 -0300

    Implementando os metodos de teste da Parte 2

--- TesteNewTalents/UnitTest1.cs
+++ TesteNewTalents/UnitTest1.cs
@@ -5,10 +5,61 @@ namespace TesteNewTalents
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(4, 5, 9)]
+        public void TestSomar(int val1, int val2, int resultado)
         {
-        
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.somar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(4, 5, 20)]
+        public void TestMultiplicar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.mutiplicar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(5, 5, 1)]
+        public void TestDividir(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.dividir(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 4)]
+        [InlineData(6, 5, 1)]
+        public void TestSubtrair(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.subtrair(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Fact]
+        public void TestDivisaoPorZero()
+        {
+            Calculadora calc = new Calculadora();
+
+            Assert.Throws<DivideByZeroException>(() => calc.dividir(3, 0));
+
         }
     }
 }
